4ba7a7f8d1a8f782119e6bf4527d68775ace7cb6
Trevor Dorsey <tsdorsey@gmail.com>
2018-05-23 18:48:24 -0700

    Search by name checkpoint

--- api.js
+++ api.js
@@ -5,7 +5,35 @@ var pd = new Pokedex.Pokedex({
 
 function getPokemon(id) {}
 
-function searchByName(query) {}
+function searchByName(query) {
+  return pd.getPokemonsList().then(function(result) {
+    var found = [];
+    for(var rI = 0; rI < result.results.length; rI++) {
+      var score = 0;
+      var item = result.results[rI];
+      if(item.name.includes(query)) {
+        score += 1;
+        if(item.name.startsWith(query)) {
+          score += 1;
+        }
+        score += query.length / item.name.length;
+      }
+
+      if(score > 0) {
+        found.push([score, item.name]);
+      }
+    }
+    found.sort();
+    found.reverse();
+
+    var promises = [];
+    for(var pI = 0; pI < found.length; pI++) {
+      promises.push(pd.getPokemonByName(found[pI][1]));
+    }
+
+    return Promise.all(promises);
+  });
+}
 
 function createTeam(name) {}
 function getTeams() {}
